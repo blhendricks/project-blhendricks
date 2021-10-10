ba1e28779de0627177c3c8a7291acf4772516614
Bryan <bryanhendricks1996@gmail.com>
2021-10-10 15:11:10 -0400

    Added additional comments

--- Event_Generator.jl
+++ Event_Generator.jl
@@ -127,4 +127,5 @@ function generate_eventlist_cylinder(n_events, Emin, Emax, volume,
 end
 
 vol = Dict("fiducial_rmin" => 0, "fiducial_rmax" => 5, "fiducial_zmin" => -2.7, "fiducial_zmax" => 0)
-data, att = generate_eventlist_cylinder(10, 1e18, 1e19, vol, "ccnc")
+data, att = generate_eventlist_cylinder(n_events=10, Emin=1e18, Emax=1e19,
+    volume=vol, interaction_type="ccnc")
