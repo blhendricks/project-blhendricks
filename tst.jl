286aa7ef0ad3b21c055ddb84514e2a980b96398f
Bryan <bryanhendricks1996@gmail.com>
2021-12-09 13:05:15 -0500

    Updated to new files

--- tst.jl
+++ tst.jl
@@ -1,11 +1,20 @@
 module tst
 using BenchmarkTools
 
-include("Event_Generator.jl")
-import .Event_Gen
+include("Event_Generator_symbols.jl")
+import .Event_Gen_sym
 
 #this describes the volume of the simulation
-vol = Dict("fiducial_rmin" => 0, "fiducial_rmax" => 4000, "fiducial_zmin" => -2700, "fiducial_zmax" => 0)
+#vol = Dict("fiducial_rmin" => 0.0, "fiducial_rmax" => 5000.0, "fiducial_zmin" => -2700, "fiducial_zmax" => 0.0)
+
+#num_list = [1, 10, 100, 1000]
+#benchmarks_0 = benchmark_my_funciton(Event_gen.generate_eventlist_cylinder, num_list)
+
+#plt0 = scatter(benchmarks_0.num_list, log10.(benchmarks_0.num_list./benchmarks_0.times_list), xscale=:log10, label=:none)
+#		xlabel!(plt0, "Size of Array")
+#		ylabel!(plt0, "log_10 (Evals/s)")
+#		title!(plt0,"Benchmarks for my_function_0_args")
+#		plt0
 
 # The paramets in order of the following input are n_events, Emin, Emax,
 # volume (Dict, found above), and interaction_type ("cc", "nc", or "ccnc")
