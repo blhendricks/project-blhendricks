9903dcda22f1d3fd0ccd11632c1acb79f589d08d
Bryan <bryanhendricks1996@gmail.com>
2021-12-08 19:37:28 -0500

    Modified distance to be in meters rather than km

--- tst.jl
+++ tst.jl
@@ -2,7 +2,7 @@ include("Event_Generator.jl")
 import .Event_Gen
 
 #this describes the volume of the simulation
-vol = Dict("fiducial_rmin" => 0, "fiducial_rmax" => 4, "fiducial_zmin" => -2.7, "fiducial_zmax" => 0)
+vol = Dict("fiducial_rmin" => 0, "fiducial_rmax" => 4000, "fiducial_zmin" => -2700, "fiducial_zmax" => 0)
 # The paramets in order of the following input are n_events, Emin, Emax,
 # volume (Dict, found above), and interaction_type ("cc", "nc", or "ccnc")
 data, att = Event_Gen.generate_eventlist_cylinder(10, 1e19, 1e19, vol, "ccnc")
